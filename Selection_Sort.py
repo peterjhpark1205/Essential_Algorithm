398b943f020412cd4198945e33f3d63c3237e4d3
Peter.Park <59194004+peterjhpark1205@users.noreply.github.com>
2021-04-05 09:46:32 +0900

    Update Selection_Sort.py

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,8 +1,8 @@
 # Selection Sort
 
-# 왜.. 왜!!! 값이 계속 삐끗하는 것인가... 해결이 시급하도다.. 밥먹고 
+# 설명: 리스트에서 최솟값을 찾아 맨 앞으로 보낸다. 한번 수행하면 index를 다음으로 옮겨가 거기서부터의 최솟값을 맨 앞에서 그 다음 자리(index가 있는자리)에 놓는다.
 
-
+# 잘못된 코드: 계속 index2를 따라가면 min값을 찾지 못한다. 즉 '내가 갖고 있는 값(기준점)'을 잡아줘야 한다.
 def selection_sort(list):
 
     for index in range(len(list) - 1):
@@ -10,6 +10,22 @@ def selection_sort(list):
         for index2 in range(index, len(list) - 1):
             if list[index2] < list[index2 + 1]:
                 temp = index2
+            else:
+                temp = index2 + 1
+        list[index], list[temp] = list[temp], list[index]
+
+    return list
+
+
+
+# 수정된 코드
+def selection_sort(list):
+
+    for index in range(len(list) - 1):
+        temp = index
+        for index2 in range(index, len(list) - 1):
+            if list[temp] < list[index2 + 1]:
+                temp = temp
             else:
                 temp = index2 + 1
         list[index], list[temp] = list[temp], list[index]
